fa45818821c4692391054894d24b3c86c62d3724
Kyle Kellums <Kyle@Techs-MacBook-Pro.local>
2017-06-06 13:16:38 -0500

    DataFactory.js empty functions

---
+++ app/factories/DataFactory.js
@@ -0,0 +1,36 @@
+"use strict";
+
+app.factory("DataFactory", () => {
+
+
+	const getPinList = () => {
+
+	}
+
+	const getBoardList = () => {
+
+	}
+
+	const editPin = (uid, boardId, pinId) => {
+
+	}
+
+	const deleteBoard = (uid, boardId) => {
+
+	}
+
+	const editBoard = (boardId) => {
+
+	}
+
+	const addPin = (obj) => {
+
+	}
+
+	const addBoard = (boardObj) => {
+
+	}
+
+// getPin ???
+// getBoard ???
+});
