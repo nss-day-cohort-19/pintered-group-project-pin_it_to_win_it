33de696709cb66959ce5b3d7bdf812ebcc165323
Kathleen Weisensel <kweisensel@gmail.com>
2017-06-08 11:35:29 -0500

    create board functionality working yay

--- app/controllers/CreateBoardCtrl.js
+++ app/controllers/CreateBoardCtrl.js
@@ -2,6 +2,20 @@
 
 app.controller("CreateBoardCtrl", function($scope, DataFactory, $location, AuthFactory, $routeParams) {
 
+	let user = AuthFactory.getUser();
 
+	$scope.boards = {
+		title: "",
+		uid: user
+	};
+
+	$scope.addBoard = function() {
+		// add a new board
+		console.log("$scope.boards", $scope.boards);
+		DataFactory.addBoard($scope.boards)
+		.then( (data) => {
+			$location.path("/profile");
+		});
+	};
 
 });
--- app/controllers/ProfileCtrl.js
+++ app/controllers/ProfileCtrl.js
@@ -1,11 +1,17 @@
 "use strict";
 console.log("Profile Ctrl loads");
 
-app.controller('ProfileCtrl', function($scope, DataFactory, $location, AuthFactory) {
+app.controller('ProfileCtrl', function($scope, DataFactory, $location, AuthFactory, $routeParams) {
 	// pass SearchTermData (from FilterFactory) into above function once created
 
 	// $scope.searchText = SearchTermData; (add once SearchTermData is created)
 	let user = AuthFactory.getUser();
+	console.log("user from profile ctrl", user);
+
+	$scope.boards = {
+		name: "",
+		uid: user
+	};
 
 	$scope.getBoardList = function() {
 		// get the user's board
@@ -16,20 +22,6 @@ app.controller('ProfileCtrl', function($scope, DataFactory, $location, AuthFacto
 		});
 	};
 
-	$scope.boards = {
-		name: "",
-		uid: user
-	};
-
-	$scope.addBoard = function() {
-		// add a new board
-		console.log("$scope.boards", $scope.boards);
-		DataFactory.addBoard($scope.boards)
-		.then( (data) => {
-			$location.path("/profile");
-		});
-		$scope.boards = {};
-	};
 
 $scope.getBoardList();
 
