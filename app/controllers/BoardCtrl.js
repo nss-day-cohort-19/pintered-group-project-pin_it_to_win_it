d1120b9636ab2341191522be24e401170e5456ae
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-06-08 11:24:51 -0500

    created BoardCtrl functionality

--- app/controllers/BoardCtrl.js
+++ app/controllers/BoardCtrl.js
@@ -6,40 +6,61 @@ app.controller("BoardCtrl", function($scope, DataFactory, $location, AuthFactory
 	console.log("$routeParams", $routeParams.boardID);
 // This view has the following buttons: "Create Pin", "Delete Pin", "Delete Board".
 
-$scope.newPin = { //this will
-		boardID: [],
-		pinID: "",
-		title: "",
-		UID: "",
-		url: ""
-	};
+	// $scope.newPin = { //this will be on the createPinCtrl
+	// 	boardID: "",
+	// 	pinID: "",
+	// 	title: "",
+	// 	UID: "",
+	// 	url: ""
+	// };
+	$scope.pinsForBoard = [];
+	$scope.boardID = $routeParams.boardID;
 
-	$scope.getPinList = () => {
-		console.log("called???");
-		DataFactory.getPinList()
+	// $scope.addPin = function(){
+	// 	DataFactory.addPin($scope.newPin);
+	// };
+	//move to createPinCtrl
+
+	$scope.getBoardPins = function(){
+		DataFactory.getBoardPins($routeParams.boardID)
 		.then( (pins) => {
-			$scope.pins = pins;
-			console.log("the pins", $scope.pins);
+			console.log("the pins", pins);
+			$scope.pinsForBoard = pins;
 		});
 	};
 
-	$scope.getBoard = () => {
-		console.log("boards called???");
-		DataFactory.getBoard($routeParams.boardID)
-		.then( (boards) => {
-			$scope.boards = boards;
-			$scope.boardID = $routeParams.boardID;
-			console.log("the boards", $scope.boards);
-			console.log("a boardID", $scope.boardID);
+	$scope.deleteBoard = function(){
+		DataFactory.deleteBoard($scope.boardID)
+		.then ( (data) => {
+			$scope.pinsForBoard.forEach(function(currVal){
+				console.log("currVal.pinID", currVal.pinID);
+				DataFactory.deletePin(currVal.pinID);
+			});
+			$location.path('/profile');
 		});
 	};
 
-	$scope.addPin = function(){
-		DataFactory.addPin($scope.newPin);
+	$scope.deletePin = function(pinID){
+		DataFactory.deletePin(pinID).
+		then( (data) => {
+			$scope.getBoardPins();
+		});
 	};
 
+	// $scope.getBoard = () => {
+	// 	console.log("boards called???");
+	// 	DataFactory.getBoard($routeParams.boardID)
+	// 	.then( (boards) => {
+	// 		$scope.boards = boards;
+	// 		$scope.boardID = $routeParams.boardID;
+	// 		console.log("the boards", $scope.boards);
+	// 		console.log("a boardID", $scope.boardID);
+	// 	});
+	// };
 
-	$scope.getPinList();
-	$scope.getBoard();
+
+
+	$scope.getBoardPins();
+	// $scope.getBoard();
 
 });
