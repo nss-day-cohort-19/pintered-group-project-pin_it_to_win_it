ac833a37cf61db8ecea19317c1bbdfa4ef538a39
Kyle Kellums <Kyle@Techs-MacBook-Pro.local>
2017-06-08 14:37:45 -0500

    createPinCtrl and partial

--- app/controllers/CreatePinCtrl.js
+++ app/controllers/CreatePinCtrl.js
@@ -1,6 +1,22 @@
 "use strict";
 
-app.controller("CreatePinCtrl", function(){
+app.controller("CreatePinCtrl", function($scope, DataFactory, $location, AuthFactory, $routeParams){
 
+	let user = AuthFactory.getUser();
+
+	$scope.newPin = {
+		boardID: $routeParams.boardID,
+		pinID: "",
+		title: "",
+		uid: user,
+		url: ""
+	};
+
+	$scope.addPin = function(){
+		DataFactory.addPin($scope.newPin)
+		.then( (data) => {
+			$location.path("./boards/{$routeParams.boardID}");
+		});
+	};
 
 });
